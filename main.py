61999709b9486e38855e4a9386fd4761810d6ede
Duong Le <duong.le@moreh.com.vn>
2024-07-15 17:14:44 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+####################################### IMPORT #################################
+import json
+import pandas as pd
+from PIL import Image
+from loguru import logger
+import sys
+
+from fastapi import FastAPI, File, status
+from fastapi.responses import RedirectResponse
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import HTTPException
+
+from io import BytesIO
+
+from app import get_image_from_bytes
+# from app import detect_sample_model
+# from app import add_bboxs_on_img
+from app import get_bytes_from_image
+from app import get_json_prediction_result
+
+####################################### logger #################################
+
+logger.remove()
+logger.add(
+    sys.stderr,
+    colorize=True,
+    format="<green>{time:HH:mm:ss}</green> | <level>{message}</level>",
+    level=10,
+)
+logger.add("log.log", rotation="1 MB", level="DEBUG", compression="zip")
+
+###################### FastAPI Setup #############################
+
+# title
+app = FastAPI(
+    title="Capstone project AI model FastAPI",
+    description="""Predict waste items of image""",
+    version="2024.7.15",
+)
+
+# This function is needed if you want to allow client requests 
+# from specific domains (specified in the origins argument) 
+# to access resources from the FastAPI server, 
+# and the client and server are hosted on different domains.
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8008",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+def save_openapi_json():
+    '''This function is used to save the OpenAPI documentation 
+    data of the FastAPI application to a JSON file. 
+    The purpose of saving the OpenAPI documentation data is to have 
+    a permanent and offline record of the API specification, 
+    which can be used for documentation purposes or 
+    to generate client libraries. It is not necessarily needed, 
+    but can be helpful in certain scenarios.'''
+    openapi_data = app.openapi()
+    # Change "openapi.json" to desired filename
+    with open("openapi.json", "w") as file:
+        json.dump(openapi_data, file)
+
+# redirect
+@app.get("/", include_in_schema=False)
+async def redirect():
+    return RedirectResponse("/docs")
+
+@app.get('/healthcheck', status_code=status.HTTP_200_OK)
+def perform_healthcheck():
+    '''
+    It basically sends a GET request to the route & hopes to get a "200"
+    response code. Failing to return a 200 response code just enables
+    the GitHub Actions to rollback to the last version the project was
+    found in a "working condition". It acts as a last line of defense in
+    case something goes south.
+    Additionally, it also returns a JSON response in the form of:
+    {
+        'healtcheck': 'Everything OK!'
+    }
+    '''
+    return {'healthcheck': 'Everything OK!'}
+
+######################### Support Func #################################
+
+
+######################### MAIN Func #################################
+@app.post("/json_detection")
+def img_object_detection_to_json(file: bytes = File(...)):
+    """
+    Object Detection from an image.
+
+    Args:
+        file (bytes): The image file in bytes format.
+    Returns:
+        dict: JSON format containing the Objects Detections.
+    """
+    # Step 1: Initialize the result dictionary with None values
+    # result={'detect_objects': "None"}
+    result={'detect_objects': "teststs"}
+    # Step 2: Convert the image file to an image object
+    input_image = get_image_from_bytes(file)
+    predict_result = get_json_prediction_result(input_image)
+    # Step 5: Logs and return
+    logger.info("results: {}", result)
+    return result
